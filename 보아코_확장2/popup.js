d6cb5670e8b83a408750c9b817d990b6e14b03c7
jinsw53 <jin.s.w53@gmail.com>
2026-07-06 18:34:16 +0900

    Update popup.js

--- 보아코_확장2/popup.js
+++ 보아코_확장2/popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   archiveBtn.addEventListener('click', () => {
     // 실제 운영 중인 아카이브 주소로 변경해줘
-    const archiveUrl = "https://jinsw53.github.io/kingofleague/test/%EB%A0%88%EC%9D%B4%EC%95%84%EC%9B%83-%EB%A1%9C%EA%B7%B8%EC%9D%B8.html#";
+    const archiveUrl = "https://boakoarchive.co.kr/";
     
     // 새 탭으로 아카이브 열기
     chrome.tabs.create({ url: archiveUrl });
